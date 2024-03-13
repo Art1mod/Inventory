6cb0b066996f85b03705c48e69857d5641fb29dd
Artem <artmod2112@gmail.com>
2024-03-13 12:39:39 +0200

    ItemBase class

--- InventorySystem/Source/InventorySystem/Private/Items/ItemBase.cpp
+++ InventorySystem/Source/InventorySystem/Private/Items/ItemBase.cpp
@@ -3,3 +3,52 @@
 
 #include "Items/ItemBase.h"
 
+UItemBase::UItemBase()
+{
+
+}
+
+UItemBase* UItemBase::CreateItemCopy() const
+{
+	UItemBase* ItemCopy = NewObject<UItemBase>(StaticClass());
+	
+	ItemCopy->Quantity = this->Quantity;
+	ItemCopy->ItemID = this->ItemID;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->ItemQuality = this->ItemQuality;
+	ItemCopy->ItemStatistics = this->ItemStatistics;
+	ItemCopy->ItemTextData = this->ItemTextData;
+	ItemCopy->ItemNumericData = this->ItemNumericData;
+	ItemCopy->ItemAssetData = this->ItemAssetData;
+
+	return ItemCopy;
+}
+
+void UItemBase::SetQuantity(const int32 NewQuantity)
+{
+	if (NewQuantity != Quantity)
+	{
+		Quantity = FMath::Clamp(NewQuantity, 0, ItemNumericData.bIsStackable ? ItemNumericData.MaximumStackSize : 1);
+		
+		// TO DO
+		//if (OwningInventory)
+		//{
+		//	if (Quantity <= 0) 
+		//	{
+		//		OwningInventory->RemoveItem(this);
+		//	}
+		//}
+		
+	}
+
+
+}
+
+void UItemBase::Use(AInventorySystemCharacter* Character)
+{
+}
+
+bool UItemBase::operator==(const FName& OtherID) const
+{
+	return ItemID == OtherID;	
+}
--- InventorySystem/Source/InventorySystem/Public/Items/ItemBase.h
+++ InventorySystem/Source/InventorySystem/Public/Items/ItemBase.h
@@ -4,14 +4,86 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "Data/ItemDataStructs.h"
 #include "ItemBase.generated.h"
+
 
 /**
  * 
  */
+
+class AInventorySystemCharacter;
+class UInventoryComponent;
+
 UCLASS()
 class INVENTORYSYSTEM_API UItemBase : public UObject
 {
 	GENERATED_BODY()
 	
+public: 
+
+	//==================================================================================
+	// PROPERTIES & VARIABLES
+	//==================================================================================
+	
+	// TO DO
+	//UPROPERTY()
+	//UInventoryComponent* OwningInventory;
+
+	UPROPERTY(VisibleAnywhere, Category = "Item Data", meta = (UIMin=1, UIMax=100))
+	int32 Quantity; 
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FName ItemID;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemTextData ItemTextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData ItemNumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemAssetData ItemAssetData;
+
+	//==================================================================================
+	// FUNCTIONS
+	//==================================================================================
+
+	UItemBase();
+
+	//this function's used for when you add an item to the inventory,
+	//you need a possibility to create a brand new item copy to avoid bugs
+	UFUNCTION(Category = "Item")
+	UItemBase* CreateItemCopy() const ;
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemStackWeight() const { return Quantity * ItemNumericData.Weight; }
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemSingleWeight() const { return ItemNumericData.Weight; }
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE bool IsFullItemStack() const { return Quantity == ItemNumericData.MaximumStackSize; }
+
+	UFUNCTION(Category = "Item")
+	void SetQuantity(const int32 NewQuantity);
+
+	UFUNCTION(Category = "Item")
+	virtual void Use(AInventorySystemCharacter* Character);
+
+
+protected:
+	
+	bool operator == (const FName& OtherID) const;
+	
+
 };
